77dc834962c07ba1f66dcb5c7ab952fc822d6688
Matheus Felipe <mfasmatheus@gmail.com>
2024-05-19 14:09:25 -0300

    [UPDATE] Create Login Shared for tests

--- test/repositories/login/login_repository_test.dart
+++ test/repositories/login/login_repository_test.dart
@@ -1,29 +1,21 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:loomi_test/models/logged_user_model.dart';
 import 'package:loomi_test/repositories/login/login_repository.dart';
 import 'package:mocktail/mocktail.dart';
+
+import '../../shared/login_shared.dart';
 
 class LoginRepositoryMock extends Mock implements LoginRepository {}
 
 void main() {
   late LoginRepository repository;
 
-  LoggedUserModel mockedUserModel = LoggedUserModel(
-    authToken: "teste auth",
-    refreshToken: "teste refresh",
-  );
-
   setUp(() {
     repository = LoginRepositoryMock();
   });
 
   group("Login", () {
-    void arrangeLoginRepositoryMock() {
-      when(() => repository.login()).thenAnswer((_) async => Future.value(mockedUserModel));
-    }
-
-    test("Test login", () async {
-      arrangeLoginRepositoryMock();
+    test("Test login repository", () async {
+      LoginShared.arrangeLoginRepositoryMock(repository);
 
       var result = await repository.login();
 
