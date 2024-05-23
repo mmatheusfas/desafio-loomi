76681c4c7fe5fc76540aa4c06daeea114a304735
Matheus Felipe <mfasmatheus@gmail.com>
2024-05-22 22:50:57 -0300

    [UPDATE] Refactor home view and tab view

--- lib/features/tab_view/tab_view.dart
+++ lib/features/tab_view/tab_view.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:loomi_test/features/home/home_view.dart';
 import 'package:loomi_test/support/components/gradient_background.dart';
-import 'package:loomi_test/support/utils/app_assets.dart';
 import 'package:loomi_ui/loomi_ui.dart';
+
+import '../../support/utils/app_assets.dart';
 
 class TabView extends StatelessWidget {
   const TabView({super.key});
@@ -15,17 +16,25 @@ class TabView extends StatelessWidget {
         initialIndex: 0,
         child: Scaffold(
           appBar: AppBar(
+            automaticallyImplyLeading: false,
             backgroundColor: AppColors.lightYellow,
             centerTitle: true,
             title: Image.asset(AppAssets.icImperioLogo),
-            bottom: HomeTabBar(),
+            bottom: HomeTabBar(
+              tabItems: const [
+                TabBarItem(imagePath: AppAssets.icWhistle, label: "Todos"),
+                TabBarItem(imagePath: AppAssets.icSoccer, label: "Futebol"),
+                TabBarItem(imagePath: AppAssets.icBasketball, label: "Basquete"),
+                TabBarItem(imagePath: AppAssets.icEsports, label: "E-Sports"),
+              ],
+            ),
           ),
           body: const TabBarView(
             children: [
               HomeView(),
-              HomeView(),
-              HomeView(),
-              HomeView(),
+              Center(child: Text("Futebol")),
+              Center(child: Text("Basquete")),
+              Center(child: Text("E-sports")),
             ],
           ),
         ),
